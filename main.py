b135c5530b6ea2e275fd0fcf1441b147dd0d7ba9
jake <jakewdr@proton.me>
2024-10-04 11:08:37 +0100

    Reformatting and error handling

--- main.py
+++ main.py
@@ -7,75 +7,55 @@ import urllib.parse
 from spotipy.oauth2 import SpotifyOAuth
 
 def main() -> None:
-    
-    # Loading secret variables ->
-    
-    secretsDictionary: dict = loadJson("secrets.json")
-    
-    SPOTIFYCLIENTID: str = secretsDictionary["Spotify"]["clientID"].strip()
-    SPOTIFYCLIENTSECRET: str = secretsDictionary["Spotify"]["clientSecret"].strip()
-    
-    LASTFMAPIKEY: str = secretsDictionary["lastfm"]["apiKey"].strip()
-    LASTFMSHAREDSECRET: str = secretsDictionary["lastfm"]["sharedSecret"].strip()
-    LASTFMUSERNAME: str = secretsDictionary["lastfm"]["username"].strip()
-    LASTFMPASSWORD = hashlib.md5(secretsDictionary["lastfm"]["password"].strip().encode("utf-8")).hexdigest()
-    # Making these empty strings for later ->
-    
-    trackName: str = ""
-    trackArtist: str = ""
-    trackAlbum: str = ""
-    
-    lastTrackID: str = ""
-    scrobbledTracks: set = set()
-    
-    numberOfTimesRan: int = 0
+    secretsDictionary = loadJson("secrets.json")
 
-    # Loading automatic edits ->
+    SPOTIFYCLIENTID = secretsDictionary["Spotify"]["clientID"].strip()
+    SPOTIFYCLIENTSECRET = secretsDictionary["Spotify"]["clientSecret"].strip()
     
-    automaticEdits: dict = loadJson("automaticEdits.json")
+    LASTFMAPIKEY = secretsDictionary["lastfm"]["apiKey"].strip()
+    LASTFMSHAREDSECRET = secretsDictionary["lastfm"]["sharedSecret"].strip()
 
-    # Getting the authentication done ->
+    lastTrackID = ""
+    scrobbledTracks = set()
+
+    automaticEdits = loadJson("automaticEdits.json")
 
     token = getToken(SPOTIFYCLIENTID, SPOTIFYCLIENTSECRET)
     lastFMToken = getLastFMToken(LASTFMAPIKEY)
     lastFMsessionKey = getSessionKey(LASTFMAPIKEY, LASTFMSHAREDSECRET, lastFMToken)
 
-    # Getting the Lastfm
-
-    # Get currently Playing Track ->
-    
     while True:
-        currentTrack = token.current_user_playing_track()
-        if (currentTrack and currentTrack["is_playing"]) == True:       
-            trackName = currentTrack["item"]["name"]
-            trackArtist = currentTrack["item"]["artists"][0]["name"]
-            trackAlbum = currentTrack["item"]["album"]["name"]
-            trackID = currentTrack['item']['id']
-            
-            trackProgressMS = currentTrack["progress_ms"]
-            trackProgressSeconds = trackProgressMS / 1000
-            trackDurationSeconds = currentTrack["item"]["duration_ms"] / 1000
-            
-            print(f"Name: {trackName}\nArtist: {trackArtist}\nAlbum: {trackAlbum}\nCurrent Progress: {str(trackProgressSeconds)}\n")
-
-            if (trackProgressSeconds >= 240 or 
-            trackProgressSeconds >= trackDurationSeconds / 2):
-                if trackID  not in scrobbledTracks:
-                    scrobbleTrack(trackName, trackArtist, trackAlbum, LASTFMAPIKEY, automaticEdits, LASTFMSHAREDSECRET, lastFMToken, lastFMsessionKey)
-                scrobbledTracks.add(trackID)
+        try:
+            currentTrack = token.current_user_playing_track()
+            if currentTrack and currentTrack["is_playing"]:       
+                trackName = currentTrack["item"]["name"]
+                trackArtist = currentTrack["item"]["artists"][0]["name"]
+                trackAlbum = currentTrack["item"]["album"]["name"]
+                trackID = currentTrack['item']['id']
+                trackProgressMS = currentTrack["progress_ms"]
+                trackProgressSeconds = trackProgressMS / 1000
+                trackDurationSeconds = currentTrack["item"]["duration_ms"] / 1000
                 
-            if lastTrackID != trackID or trackProgressMS < 15000:  # Reset if the track ID changes or restarts
-                lastTrackID = trackID
-                scrobbledTracks.clear()
-        else:
-            print("No track playing!")
+                print(f"Name: {trackName}\nArtist: {trackArtist}\nAlbum: {trackAlbum}\nCurrent Progress: {trackProgressSeconds:.2f}\n")
+
+                if trackID not in scrobbledTracks and (trackProgressSeconds >= 240 or trackProgressSeconds >= trackDurationSeconds / 2):
+                    scrobbleTrack(trackName, trackArtist, trackAlbum, LASTFMAPIKEY, automaticEdits, LASTFMSHAREDSECRET, lastFMToken, lastFMsessionKey)
+                    scrobbledTracks.add(trackID)
+
+                if lastTrackID != trackID or trackProgressMS < 15000:  # Reset if the track ID changes or restarts
+                    lastTrackID = trackID
+                    scrobbledTracks.clear()
+            else:
+                print("No track playing!")
+        except Exception as e:
+            print(f"Error: {e}")
+        
         time.sleep(10)
 
 def scrobbleTrack(track, artist, album, apiKey, edits, secret, token, sessionKey):
     newTags = tagFixer(track, artist, album, edits)
     url = 'http://ws.audioscrobbler.com/2.0/'
 
-    # Prepare the data
     data = {
         'method': 'track.scrobble',
         'api_key': apiKey,
@@ -83,38 +63,37 @@ def scrobbleTrack(track, artist, album, apiKey, edits, secret, token, sessionKey
         'artist': newTags[0],
         'track': newTags[1],
         'album': newTags[2],
-        'timestamp': time.time(),
+        'timestamp': int(time.time()),
         'format': 'json'
     }
 
     signature = generateSignature(data, secret)
     data['api_sig'] = signature
     
-    response = requests.post(url, data=data)
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        print("Track scrobbled successfully.")
+    except requests.RequestException as e:
+        print(f"Failed to scrobble track: {e}")
 
 def generateSignature(params, apiSecret):
-    # Sort parameters and concatenate them
     keys = sorted(params.keys())
-    signature = ''.join(f'{key}{params[key]}' for key in keys)
-    signature += apiSecret
+    signature = ''.join(f'{key}{params[key]}' for key in keys) + apiSecret
     return hashlib.md5(signature.encode('utf-8')).hexdigest()
 
 def getToken(clientID: str, clientSecret: str) -> dict: 
-    token = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(
-            client_id=clientID,
-            client_secret=clientSecret,
-            redirect_uri="https://github.com/jakewdr/middlefm/",
-            scope="user-read-playback-state"
-        )
-    )
-    return token
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=clientID,
+        client_secret=clientSecret,
+        redirect_uri="https://github.com/jakewdr/middlefm/",
+        scope="user-read-playback-state"
+    ))
 
 def getLastFMToken(apiKey):
     authUrl = f'http://last.fm/api/auth/?api_key={apiKey}&cb={urllib.parse.quote("https://github.com/jakewdr/middlefm/")}'
     print(f'Visit this URL to authenticate: {authUrl}')
-    token = input("Paste the text after in the address bar after 'github.com/jakewdr/middlefm?token='")
-
+    token = input("Paste the text after 'github.com/jakewdr/middlefm?token=': ")
     return token
 
 def getSessionKey(apiKey, apiSecret, tokenValue):
@@ -125,14 +104,16 @@ def getSessionKey(apiKey, apiSecret, tokenValue):
         'token': tokenValue,
         'format': 'json'
     }
-    params["api_sig"] = f"api_key{params['api_key']}methodauth.getSessiontoken{tokenValue}{apiSecret}"
-    params["api_sig"] = hashlib.md5(params["api_sig"].encode('utf-8')).hexdigest()
-    # Make the request
-    response = requests.post(url, data=params)
-    sessionData = response.json()
-    sessionKey = sessionData['session']['key']
-
-    return sessionKey
+    params["api_sig"] = generateSignature(params, apiSecret)
+    
+    try:
+        response = requests.post(url, data=params)
+        response.raise_for_status()
+        sessionData = response.json()
+        return sessionData['session']['key']
+    except requests.RequestException as e:
+        print(f"Failed to get session key: {e}")
+        return None
 
 def tagFixer(title: str, artist: str, album: str, edits: dict) -> list:
     if album in edits["albums"]:
@@ -168,11 +149,15 @@ def tagFixer(title: str, artist: str, album: str, edits: dict) -> list:
             if currentSong["newAlbum"] != "":
                 album = currentSong["newAlbum"]
     print([title, album, artist])
-    return [title, artist, album]    
+    return [title, artist, album]
 
 def loadJson(jsonFileName: str) -> dict:
-    with open(jsonFileName, 'r') as file:
-        return dict(json.load(file))
+    try:
+        with open(jsonFileName, 'r') as file:
+            return json.load(file)
+    except Exception as e:
+        print(f"Failed to load JSON file {jsonFileName}: {e}")
+        return {}
 
 if __name__ == "__main__":
     main()
